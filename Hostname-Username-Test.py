bfdb68b5785c085efb8f37f20029d481a1fd148b
pagenw142 <pagenw142@gmail.com>
2022-03-30 21:10:42 -0400

    Create Hostname-Username-Test.py

---
+++ Hostname-Username-Test.py
@@ -0,0 +1,17 @@
+import argparse
+
+#set up argument parsing
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("hostname", help="Hostname can be one or many hostnames. Separate multiple hostnames by comma")
+PARSER.add_argument("username", help="Username to connect to the hosts.")
+
+ARGS = PARSER.parse_args()
+
+#assign variables
+#HOSTS = (ARGS.hostname).split(",") #split the hostname input on comma
+#USERNAME = ARGS.username #username will be the same for each host
+
+#if not USERNAME:
+#    USERNAME = admin
+
+print('The hostname submitted was: ' + hostname + ' and the username was: ' + username)
