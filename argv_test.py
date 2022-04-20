425c16860d881a1092557e33a516273cc695eb78
pagenw142 <pagenw142@gmail.com>
2022-04-20 12:05:03 -0400

    argv_test.py
    Hellp

---
+++ argv_test.py
@@ -0,0 +1,10 @@
+import sys
+
+#assign variables
+hostname = sys.argv[1]
+username = sys.argv[2]
+
+#if not USERNAME:
+#    USERNAME = admin
+
+print('The hostname submitted was: ' + hostname + ' and the username was: ' + username)
