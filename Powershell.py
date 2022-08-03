d7b34d3970a1241f1b8ac65523baf117b754dcf8
pagenw142 <pagenw142@gmail.com>
2022-08-03 09:57:25 -0400

    Update Powershell.py

--- Powershell.py
+++ Powershell.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def run(self, cmd):
-    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    completed = subprocess.run(["powershell", "-Command"], capture_output=True)
     return completed
 
 
