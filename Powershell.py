f35ee823f234ab927212fe2a331e06969f8dca45
pagenw142 <pagenw142@gmail.com>
2022-08-03 09:15:57 -0400

    Update Powershell.py

--- Powershell.py
+++ Powershell.py
@@ -13,12 +13,4 @@ if __name__ == '__main__':
         print("An error occured: %s", hello_info.stderr)
     else:
         print("Hello command executed successfully!")
-    
-    print("-------------------------")
-    
-    bad_syntax_command = "Write-Hst 'Incorrect syntax command!'"
-    bad_syntax_info = run(bad_syntax_command)
-    if bad_syntax_info.returncode != 0:
-        print("An error occured: %s", bad_syntax_info.stderr)
-    else:
-        print("Bad syntax command executed successfully!")
+  
