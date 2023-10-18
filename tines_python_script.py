0ca75ae79bf15429473aac6dd282c11090f705a3
pagenw142 <pagenw142@gmail.com>
2023-10-18 08:34:01 -0400

    Create tines_python_script.py

---
+++ tines_python_script.py
@@ -0,0 +1,4 @@
+def main(input):
+  hostname = input["hostname"]
+  username = input["username"]
+  return { "result": 'The hostname submitted was: ' + hostname + ' and the username was: ' + username}
